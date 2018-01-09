528d547f5678f35a5340cf11855f05273d48879e
Phil S <philipstubbs13@gmail.com>
2018-01-08 19:06:12 -0600

    cleaned up comments in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
   //Create an array of strings, each one related to a topic (athletes). Save it to a variable called topics.
   var topics = ["lonzo ball", "ricky rubio", "jackie robinson", "draymond green", "javale mcgee", "jimmy garoppolo", "stephen curry" ,"babe ruth" ,"lamelo ball" , "shaquille o'neal" , "giannis antetokounmpo"]
+  //Create a variable for the button that the user can create and then click to display gifs from the GIPHY API.
   var athleteBtn;
+  //Create variable for gif image.
   var athleteImage;
 
   function createButtons() {
@@ -25,8 +27,7 @@
   }
 }
 
-
-  //When you click an athelete button...
+  //When you click an athlete button...
   //displayAthleteImages function re-renders the HTML to display the appropriate content
   function displayAthleteImages() {
     //Each time an athlete button is clicked and data is retrieved, empty out the columns in the results-div.
@@ -36,10 +37,10 @@
     $("#click-to-play-text").empty();
 
     var athlete = $(this).attr("data-name");
-    //Construct our query URL to access and obtain data from the giphy API.
+    //Construct our query URL to access and obtain data from the GIPHY API.
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athlete + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    //Our jQuery AJAX method. Perform AJAX GET request to the queryURL to get data from giphy API.
+    //Our jQuery AJAX method. Perform AJAX GET request to the queryURL to get data from GIPHY API.
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -55,7 +56,9 @@
 
         for (var i = 0; i < results.length; i++) {
 
-          // Only take action if the gif has an appropriate rating
+          // Only take action if the gif has an appropriate rating. 
+          // This should not matter. Did extensive testing on this site and did not find pg-13 or r rated gifs for any of the athletes tested.
+          //But, adding this if statement just in case...
           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
             //Create div element to hold gif image.. 
@@ -75,22 +78,22 @@
             athleteImage.attr("data-state", "still");
             athleteImage.addClass ("img-fluid gif border border-primary");
 
-            //Prepend rating paragraph to the div created to hold the gif image.
+            //Prepend rating paragraph to the div that was created to hold the gif image.
             gifDiv.prepend(p);
-            //Prepend gif image to the div created to hold the gif image.
+            //Prepend gif image to the div that was created to hold the gif image.
             gifDiv.prepend(athleteImage);
 
-            //Add the first three gifs retrieved from the giphy API call to the results-div-col1 column in the HTML.
+            //Add the first three gifs retrieved from the GIPHY API call to the results-div-col1 column in the HTML.
             if (i >= 0 && i < 3) {
               $("#results-div-col1").append(gifDiv);
             }
 
-            //Add the next four gifs retrieved from the giphy API call to the results-div-col2 column in the HTML.
+            //Then, add the next four gifs retrieved from the GIPHY API call to the results-div-col2 column in the HTML.
             else if (i >= 3 && i < 7) {
               $("#results-div-col2").append(gifDiv);
             }
 
-            //Finally, add the last three gifs retrieved from the giphy API call to the results-div-col3 column in the HTML.
+            //Finally, add the last three gifs retrieved from the GIPHY API call to the results-div-col3 column in the HTML.
             else {
               $("#results-div-col3").append(gifDiv);
             }
@@ -99,8 +102,9 @@
 
         }
 
+        //When the user clicks a gif in the search results section...
         $(".gif").on("click", function() {
-          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element.
           var state = $(this).attr("data-state");
           // If the clicked image's state is still, update its src attribute to what its data-animate value is.
           // Then, set the image's data-state to animate
@@ -118,10 +122,10 @@
       });
 }
 
-  //When submit button is clicked, add athlete-input from the search box to topics array.
+  //When submit/add button is clicked in the "Add your favorite athlete" section, add athlete-input from the search box to topics array.
   $("#submit-button").on("click", function(event) {
 
-    //The following code prevents the submit button from trying to submit the form.
+    //The following code prevents the submit/add button from trying to submit the form.
     //Using a form so that the user can press Enter to search instead of clicking the button.
     event.preventDefault();
     //Grab the input from the text box and change the value to lower case.
@@ -136,8 +140,8 @@
     }
 
     //If text box is empty, don't create button. Nothing should happen when user clicks Add icon.
-    else if (athleteInput == "") {
-      return;
+    else if (athleteInput === "" || athleteInput === null) {
+      return false;
     }
 
     //else if the input from the search box is not in the topics array, add athlete to topics array and create button for athlete.
@@ -156,6 +160,8 @@
 //Create click event for all elements with a class of athlete-btn.
 $(document).on("click", ".athlete-btn", displayAthleteImages);
 
+//This is the function to display the gif image that appears in the top right corner of site on md sized screens. 
+//Image appears right below header on sm or xs screens.
 function displayHeaderImage () {
     var queryURL = "https://api.giphy.com/v1/stickers/search?q=basketball&api_key=dc6zaTOxFJmzC";
     
@@ -181,7 +187,7 @@ function displayHeaderImage () {
         headerImage.attr("src", headerImageUrl);
         headerImage.addClass ("img-fluid gif");
 
-        //Prepend gif image to the div created to hold the gif image.
+        //Prepend gif image to the div that was created to hold the gif image.
         gifDiv.append(headerImage);
 
         $("#main-header-image").append(gifDiv).addClass("mt-2");
